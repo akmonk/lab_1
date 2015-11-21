def23a35ed0a7b1349a06d86cea91c60b31e5f8d
akmonk <akmonk@mail.ru>
2015-11-21 19:09:48 +0300

    structure passwd

--- whoami.c
+++ whoami.c
@@ -1,10 +1,20 @@
+// zabbix:x:502:503::/home/zabbix:/bin/bash
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdio.h>
 
 int main (void) {
  char buf[3];
+ struct user_info {
+	char name[8];
+	char pass[8];
+	int uid;
+	int guid;
+	char gecos[50];
+	char dir[50];
+	char shell[50];
+ };
  sprintf( buf, "%d", getuid());
  printf("%s\n",buf); 
-return 0;
+ return 0;
 }
