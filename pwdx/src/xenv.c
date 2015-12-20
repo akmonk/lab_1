a8fc702ef276e11aaaa7d0b3542a2db146cbbba2
akmonk <akmonk@mail.ru>
2015-12-20 17:12:37 +0300

    xenv main file

---
+++ pwdx/src/xenv.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+int main(int argc, char *argv[])
+
+{
+  int i;
+  char *pid;
+  // переданы ли pid
+  if (argc < 2)
+  {
+    printf("Ошибка. Введите pwdx pid1 [pid2]\n");
+    return 1;
+  }
+  else
+  {
+    for (i=1;i<argc;i++)
+    xenv(argv[i]);
+  }
+  return 0;
+} 
