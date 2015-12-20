379893fd253b673014bddd5abc59be0383d20290
akmonk <akmonk@mail.ru>
2015-12-20 16:49:19 +0300

    pwdx in pwdx function

--- pwdx/src/pwdx.c
+++ pwdx/src/pwdx.c
@@ -6,6 +6,28 @@
 // длина каталога
 #define MAX_PATH_SIZE 128
 
+int pwdx (char * pid)
+{
+	char *path_buf = malloc(MAX_PATH_SIZE);
+          char *buf, *s;
+          ssize_t len, buf_len;
+          buf_len = 10 + strlen(pid) + 1;
+          buf = malloc(buf_len);
+                sprintf(buf,"/proc/%s/cwd",pid);
+                len = readlink(buf, path_buf, MAX_PATH_SIZE);
+                free(buf);
+                if (len < 0)
+                {
+                        s = strerror(errno == ENOENT ? ESRCH : errno);
+                        fprintf(stderr, "%s: %s\n", pid, s);
+                        free(path_buf);
+                        return 1;
+                }
+                printf("%s: %s\n",pid,path_buf);
+                free(path_buf);
+                return 0;
+}
+
 int main(int argc, char *argv[])
 
 {
@@ -21,7 +43,8 @@ int main(int argc, char *argv[])
 	{
           for (i=1;i<argc;i++)
 	  {
-	  char *path_buf = malloc(MAX_PATH_SIZE);
+	  pwdx(argv[i]);
+/*	  char *path_buf = malloc(MAX_PATH_SIZE);
           char *buf, *s;
 	  pid = argv[i];
           ssize_t len, buf_len;
@@ -40,7 +63,7 @@ int main(int argc, char *argv[])
                 printf("%s: %s\n",pid,path_buf);
                 free(path_buf);
                 return 0;
-	}
+*/	}
 	}
 }
 
