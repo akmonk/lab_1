3e5b6d9bb79a98663a33e22ab7ee7d1e8264304c
akmonk <akmonk@mail.ru>
2015-12-20 17:11:06 +0300

    pwdx main clean + makefile

--- pwdx/src/pwdx.c
+++ pwdx/src/pwdx.c
@@ -1,34 +1,5 @@
-/*#include "stdlib.h"
-#include "string.h"
-#include "errno.h"*/
 #include "stdio.h"
 
-/*
-// длина каталога
-#define MAX_PATH_SIZE 128
-
-int pwdx (char * pid)
-{
-  char *path_buf = malloc(MAX_PATH_SIZE);
-  char *buf, *s;
-  ssize_t len, buf_len;
-  buf_len = 10 + strlen(pid) + 1;
-  buf = malloc(buf_len);
-  sprintf(buf,"/proc/%s/cwd",pid);
-  len = readlink(buf, path_buf, MAX_PATH_SIZE);
-  free(buf);
-  if (len < 0)
-  {
-    s = strerror(errno == ENOENT ? ESRCH : errno);
-    fprintf(stderr, "%s: %s\n", pid, s);
-    free(path_buf);
-    return 1;
-  }
-  printf("%s: %s\n",pid,path_buf);
-  free(path_buf);
-  return 0;
-}
-*/
 int main(int argc, char *argv[])
 
 {
